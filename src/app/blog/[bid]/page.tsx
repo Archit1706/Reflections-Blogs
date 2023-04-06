414fb79ac66e56ea190c5c1aada0c3297bc7a08f
Archit <architrathod77@gmail.com>
2023-04-06 10:57:03 +0530

    Last One

--- src/app/blog/[bid]/page.tsx
+++ src/app/blog/[bid]/page.tsx
@@ -268,7 +268,7 @@ const BlogDetail = ({ params }: any) => {
                         className="justify-center px-5 py-3 mr-3 w-max  text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
                         onClick={() => setReadFull(!readFull)}
                     >
-                        Read Complete Blog
+                        {readFull ? "Hide Blog" : "Read Full Blog"}
                     </button>
                 </div>
 
